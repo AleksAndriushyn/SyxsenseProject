1f9aaefb6cd7cf056936c4920a1953a08b65dba7
Алексей Андрюшин <aleksroebuck@gmail.com>
2021-06-25 17:33:21 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Net.WebSockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using SyxsenseProject.WebService;
+
+namespace SyxsenseProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetInformation();
+            Console.ReadKey();
+        }
+
+        public static void SendUDP(string hostNameOrAddress, int destinationPort, string data, int count)
+        {
+            IPAddress destination = Dns.GetHostAddresses(hostNameOrAddress)[0];
+            IPEndPoint endPoint = new IPEndPoint(destination, destinationPort);
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            for (int i = 0; i < count; i++)
+            {
+                socket.Connect(endPoint);
+                if (socket.Connected)
+                {
+                    SystemInfoService systemInfoService = new SystemInfoService();
+                    var response = systemInfoService.IsMachineUp(hostNameOrAddress);
+                    Console.WriteLine("Is machine online? {0}", response);
+                }
+            }
+            socket.Close();
+        }
+
+        public static void GetInformation()
+        {
+            SelectQuery query = new SelectQuery(@"Select * from Win32_OperatingSystem");
+            
+            using (ManagementObjectSearcher searcher = new ManagementObjectSearcher(query))
+            {
+                //execute the query
+                foreach (ManagementObject process in searcher.Get())
+                {
+                    //print system info
+                    Console.WriteLine("/*********Operating System Information ***************/");
+                    Console.WriteLine("{0}{1}", "OS name: ", process["Caption"]);
+                    Console.WriteLine("{0}{1}", "Time zone: ", process["CurrentTimeZone"]);
+                    Console.WriteLine("{0}{1}", "Date time: ", process["LocalDateTime"]);
+                    Console.WriteLine("{0}{1}", "Computer name: ", process["CSName"]);
+                    //Console.WriteLine("{0}{1}", ".Net version:", process[".net version"]);
+                    Int32 counter = 0;
+                    SendUDP(process["CSName"].ToString(), 41181, counter.ToString(), counter.ToString().Length);
+                }
+            }
+        }
+    }
+}
---
+++ SyxsenseProject.WebService/SystemInfoService.asmx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading;
+using System.Web;
+using System.Web.Services;
+
+namespace SyxsenseProject.WebService
+{
+    /// <summary>
+    /// Summary description for SystemInfoService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class SystemInfoService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public bool IsMachineUp(string hostName)
+        {
+            bool retVal = false;
+            try
+            {
+                Ping pingSender = new Ping();
+                PingOptions options = new PingOptions();
+                options.DontFragment = true;
+                string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+                byte[] buffer = Encoding.ASCII.GetBytes(data);
+                int timeout = 120;
+                PingReply previousReply = pingSender.Send(hostName, timeout, buffer, options);
+                if (previousReply.Status == IPStatus.Success)
+                {
+                    PingReply currentReply = pingSender.Send(hostName, timeout, buffer, options);
+                    var startTimeSpan = TimeSpan.Zero;
+                    var periodTimeSpan = TimeSpan.FromMinutes(5);
+
+                    var timer = new Timer((e) =>
+                    {
+                        if (currentReply != previousReply)
+                        {
+
+                        }
+                    }, null, startTimeSpan, periodTimeSpan);
+                    
+                    retVal = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                retVal = false;
+                Console.WriteLine(ex.Message);
+            }
+            return retVal;
+        }
+    }
+}
